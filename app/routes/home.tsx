591c1770f89f2a33c5f884679c920d7960efefb1
Lynn <clhuang224@gmail.com>
2026-04-14 13:45:57 +0800

    feat: meta

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,12 @@
-import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import type { Route } from './+types/home'
+import { Welcome } from '../welcome/welcome'
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
+    { title: '線上會議報名表' }
+  ]
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <Welcome />
 }
